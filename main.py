050d62e5087a36371ec04309e2c08658f9cd2437
Lukas Heinrich <lukas.heinrich@gmail.com>
2021-07-03 11:43:25 +0200

    tests

--- main.py
+++ main.py
@@ -2,8 +2,9 @@ from flask import jsonify
 import pyhf
 import json
 
-def regionA(request):
-    patch = request.get_json(silent=True)
+pyhf.set_backend('pytorch')
+
+def _regionA(patch):
     w = pyhf.Workspace(json.load(open('RegionA.json')))
     m = w.model(
         patches = [patch],
@@ -13,17 +14,13 @@ def regionA(request):
         },
     )
     d = w.data(m)
-    o,e = pyhf.utils.hypotest(1.0,d,m, return_expected_set=True)
+    o,e = pyhf.infer.hypotest(1.0,d,m, return_expected_set=True)
+    return {
+            'CLs_obs': float(o),
+            'CLs_exp': [float(ee) for ee in e]
+    }
 
-    return jsonify(
-        {
-            'CLs_obs': o.tolist()[0],
-            'CLs_exp': [ee.tolist()[0] for ee in e]
-        }
-    )
-
-def regionC(request):
-    patch = request.get_json(silent=True)
+def _regionC(patch):
     w = pyhf.Workspace(json.load(open('RegionC.json')))
     m = w.model(
         patches = [patch],
@@ -33,11 +30,24 @@ def regionC(request):
         },
     )
     d = w.data(m)
-    o,e = pyhf.utils.hypotest(1.0,d,m, return_expected_set=True)
+    o,e = pyhf.infer.hypotest(1.0,d,m, return_expected_set=True)
+    return {
+            'CLs_obs': float(o),
+            'CLs_exp': [float(ee) for ee in e]
+    }
+def regionA(request):
+    patch = request.get_json(silent=True)
+    return jsonify(_regionA(patch))
 
-    return jsonify(
-        {
-            'CLs_obs': o.tolist()[0],
-            'CLs_exp': [ee.tolist()[0] for ee in e]
-        }
-    )
+def regionC(request):
+    patch = request.get_json(silent=True)
+    return jsonify(_regionC(patch))
+
+if __name__ == '__main__':
+    import sys
+    import time
+    p = json.load(sys.stdin)
+    start = time.time()
+    for i in range(10):
+        print(json.dumps(_regionA(p)))
+    print((time.time()-start)/10)
